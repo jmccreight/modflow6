78f5b203367c7376e05e635926761b5b1651113a
Langevin, Christian D <langevin@usgs.gov>
2023-02-10 11:08:32 -0700

    work on tests

--- autotest/test_snf_thomas.py
+++ autotest/test_snf_thomas.py
@@ -1,27 +1,9 @@
 """
 
-Simple 4 segment network with 5 vertices
-
-
-zero-based diagram below
-v1
-o
- |
-  |
-   |  s0
-    |
-     |
-      |
-o------o------o------o
-v0     v2     v3     v4
-    s1     s2     s3
-
-ia  ja
-0   0 2
-2   1 2
-4   2 0 1 3
-8   3 2
-10
+Thomas routine example
+500 miles
+dx = 25 miles
+sinusoidal inflow
 
 """
 
@@ -31,42 +13,35 @@ import flopy
 import numpy as np
 
 
-def test_disl_simple(function_tmpdir, targets):
+def test_snf_thomas(function_tmpdir, targets):
     sim_ws = str(function_tmpdir)
     mf6 = targets["mf6"]
-    name = "snf-disl01"
+    name = "snf-thomas"
     sim = flopy.mf6.MFSimulation(
-        sim_name=name, version="mf6", exe_name=mf6, sim_ws=sim_ws,
+        sim_name=name, version="mf6", exe_name=mf6, sim_ws=sim_ws
     )
-    sim.name_file.memory_print_option = "all"
-    tdis = flopy.mf6.ModflowTdis(sim)
+    nper = 10
+    tdis_rc = [(1., 1, 1.0) for ispd in range(nper)]
+    tdis = flopy.mf6.ModflowTdis(sim, nper=nper, perioddata=tdis_rc)
     ems = flopy.mf6.ModflowEms(sim)
     snf = flopy.mf6.ModflowSnf(sim, modelname=name)
 
-    vertices = [
-        [0, 0., 0., 0.], 
-        [1, 0., 1., 0.],
-        [2, 1., 0., 0.],
-        [3, 2., 0., 0.],
-        [4, 3., 0., 0.],
-    ]
-    # icell1d fdc ncvert icvert
-    cell2d = [
-        [0, 0.5, 2, 1, 2],
-        [1, 0.5, 2, 0, 2],
-        [2, 0.5, 2, 2, 3],
-        [3, 0.5, 2, 3, 4],
-    ]
+    vertices = None
+    cell2d = None
+    nvert = None
 
-    nodes = len(cell2d)
-    nvert = len(vertices)
+    nodes = 20
+    channel_length = 500. # miles
+    segment_length = channel_length * 5280. / nodes
+    tosegment = list(range(1, nodes))
+    tosegment.append(-1)
 
     disl = flopy.mf6.ModflowSnfdisl(
         snf, 
         nodes=nodes, 
         nvert=nvert,
-        segment_length=1000.0,
-        tosegment=[2, 2, 3, -1],   # -1 gives 0 in one-based, which means outflow cell
+        segment_length=segment_length,
+        tosegment=tosegment,   # -1 gives 0 in one-based, which means outflow cell
         idomain=1, 
         vertices=vertices, 
         cell2d=cell2d,
@@ -76,13 +51,14 @@ def test_disl_simple(function_tmpdir, targets):
         snf, 
         print_flows=True,
         iseg_order=list(range(nodes)),
-        mann_n=0.04, 
+        mann_n=0.0297, 
         seg_depth=100., 
-        seg_slope=0.0001, 
+        seg_slope=1./5280., 
         x_coef=0.2
     )
 
-    flw_spd = {0: [[0, 1000.0], [1, 500.]]}
+    inflow = [125, 200, 125, 50, 50, 50, 50, 50, 50, 50]
+    flw_spd = {ispd: [[0, inflow[ispd]]] for ispd in range(10)}
     flw = flopy.mf6.ModflowSnfflw(
         snf,
         print_input=True,
@@ -93,7 +69,6 @@ def test_disl_simple(function_tmpdir, targets):
     sim.write_simulation()
 
     mfsimnamtxt = f"""BEGIN options
-  MEMORY_PRINT_OPTION ALL
 END options
 
 BEGIN timing
